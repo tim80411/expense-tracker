5db19aeab3f23e2158afd154d03df718d5c5f0ef
tim80411 <tim80411@gmail.com>
2021-05-11 19:36:49 +0800

    feat: use handlebars in Express framework

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log('Express server is running on https://localhost:300')
+})
