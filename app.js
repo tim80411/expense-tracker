e7e03a91ed51869ac17c460efa2b0e4c5e066f9b
tim80411 <tim80411@gmail.com>
2021-05-14 11:09:52 +0800

    feat: add create expense function
    1. import body-parser
    2. set up new.handlebars

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const Record = require('./models/Record')
+const Category = require('./models/Record').category
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -20,14 +22,42 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// route: add expense function
+app.get('/new', (req, res) => {
+  return Category.find()
+    .lean()
+    .then(categorys => {
+      res.render('new', { categorys })
+    })
+    .catch(err => {
+      console.error(err)
+    })
+})
+
+app.post('/', (req, res) => {
+  const { name, date, category, amount} = req.body
+  return Record.create({name, date, category, amount})
+  .then(() => {
+    res.redirect('/')
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+})
+
 // route: browse all expenses
 app.get('/', (req, res) => {
   return Record.find()
     .populate('category')
     .lean()
     .then(lists => {
-      console.log(lists)
       return res.render('index', { lists })
+    })
+    .catch(err => {
+      console.error(err)
     })
 })
 
