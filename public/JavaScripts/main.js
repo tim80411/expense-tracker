9f6b63bad298d79c771c569aff2f56a42c5c5e56
tim80411 <tim80411@gmail.com>
2021-05-18 10:43:36 +0800

    feat: add total amount function

--- public/JavaScripts/main.js
+++ public/JavaScripts/main.js
@@ -1,4 +1,19 @@
 const form = document.querySelector('.needs-validation')
+const amounts = document.querySelectorAll('.amount')
+const totalPrice = document.querySelector('.total-price')
+
+let total = 0
+
+function displayTotal() {
+  amounts.forEach(amount => {
+    total += Number(amount.innerText)
+  })
+
+  totalPrice.innerText = total
+}
+
+displayTotal()
+
 
 form.addEventListener('submit', function (event) {
   if (!form.checkValidity()) {
