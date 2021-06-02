232e1269d49085cf1763a9b50bf860c98031d597
Yiting Liao <tim80411@gmail.com>
2021-06-02 10:45:00 +0800

    feat: add user router

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 
 const home = require('./modules/home')
 const record = require('./modules/record')
+const users = require('./modules/users')
 
 const router = express.Router()
 
-router.use('/', home)
+router.use('/users', users)
 router.use('/record', record)
+router.use('/', home)
+
 
 module.exports = router
